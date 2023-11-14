ff2f399d7f706f39fe3828baa84284e7d13e1fc9
DemLoveSky <dem.loginov@gmail.com>
2023-11-14 05:01:03 +0500

    Add profile popover for Header

---
+++ src/components/common/Other/PopoverApp/props.ts
@@ -0,0 +1,8 @@
+import {ReactNode} from "react";
+import {PopoverProps} from "@mantine/core";
+
+export interface PopoverAppProps {
+    children: ReactNode;
+    popover: ReactNode;
+    popoverProps?: PopoverProps;
+}
