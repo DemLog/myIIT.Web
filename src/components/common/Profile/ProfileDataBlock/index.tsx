cbde517015598b8d03773294e495e3ec55b62bb9
DemLoveSky <dem.loginov@gmail.com>
2023-11-14 10:00:09 +0500

    Add ProfileDataBlock

--- src/components/common/Profile/ProfileDataBlock/index.tsx
+++ src/components/common/Profile/ProfileDataBlock/index.tsx
@@ -3,16 +3,22 @@ import {ProfileDataBlockProps} from "./props";
 import classes from "./ProfileDataBlock.module.css";
 
 import {Box, Text} from "@mantine/core";
+import {ProfileDataBlockItem} from "@components/Profile/ProfileDataBlock/ProfileDataBlockItem";
 
 export const ProfileDataBlock: React.FC<ProfileDataBlockProps> = (props: ProfileDataBlockProps) => {
 
     return (
-        <Box className={classes.main_container} px="lg">
-            <Box className={classes.header} pt="xs">
-                <Text c="#222222" fw={500} size="md">Личные данные</Text>
+        <Box className={classes.main_container} px="lg" py="xs">
+            <Box className={classes.header}>
+                <Text c="#222222" fw={500} size="lg">Личные данные</Text>
             </Box>
-            <Box className={classes.content} pt="xs">
-                прпрпарпрпр
+            <Box className={classes.content} pt="xs" mx="xs">
+                <ProfileDataBlockItem title="Отчество" value="Максимович" />
+                <ProfileDataBlockItem title="Электронная почта" value="lohebanyi@mail.ru" />
+                <ProfileDataBlockItem title="Страна" value="Россия" />
+                <ProfileDataBlockItem title="Статус" value="Зачислен" />
+                <ProfileDataBlockItem title="Направление обучения" value="Разработка программно-информационных систем" />
+                <ProfileDataBlockItem title="Профиль" value="09.03.04 Программная инженерия" />
             </Box>
         </Box>
     )
--- src/components/pages/Profile/index.tsx
+++ src/components/pages/Profile/index.tsx
@@ -2,13 +2,14 @@ import React, {useEffect} from "react";
 import {ProfilePageProps} from "./props";
 import classes from "./ProfilePage.module.css";
 
-import {Box, Grid} from "@mantine/core";
+import {Box} from "@mantine/core";
 import {useDisclosure, useDocumentTitle, useMediaQuery} from "@mantine/hooks";
 
 import {observer} from "mobx-react";
 import {useStores} from "@core/hooks";
 
 import {NamePageBlock} from "@components/Other/NamePageBlock";
+import {XBlock, XMasonry} from "react-xmasonry";
 
 import accountIcon from "@assets/images/icons/account_circle.png";
 import {ProfileCardBlock} from "@components/Profile/ProfileCardBlock";
@@ -34,17 +35,31 @@ const ProfilePageComponent: React.FC<ProfilePageProps> = (props: ProfilePageProp
         <Box className={classes.main_container}>
             <Box className={classes.content} mt={matchesMobile ? "xs" : 0} mb={matchesMobile ? 62 : 0}>
                 <Box className={classes.content_main}>
-                    <Grid gutter="xs">
-                        <Grid.Col><NamePageBlock title="Профиль" icon={accountIcon}/></Grid.Col>
+                    <Box className={classes.header} mb="xs" mr="xs">
+                        <NamePageBlock title="Профиль" icon={accountIcon}/>
+                    </Box>
+                    <XMasonry center={false} maxColumns={10} targetBlockWidth={400} smartUpdateCeil={1000}>
+                        <XBlock width={1}>
+                            <Box mr="xs" mb="xs">
+                                <ProfileCardBlock/>
+                            </Box>
+                        </XBlock>
 
-                        <Grid.Col span={4}><ProfileCardBlock /></Grid.Col>
-                        <Grid.Col span={8}><ProfileDataBlock /></Grid.Col>
+                        <XBlock width={2}>
+                            <Box mr="xs" mb="xs">
+                                <ProfileDataBlock />
+                            </Box>
+                        </XBlock>
 
-                        <Grid.Col span={4}><ProfileRewardBlock /></Grid.Col>
-                    </Grid>
+                        <XBlock width={1}>
+                            <Box mr="xs" mb="xs">
+                                <ProfileRewardBlock />
+                            </Box>
+                        </XBlock>
+                    </XMasonry>
                 </Box>
-                <Box className={classes.content_help_block} ml="md">
-                    <ProfileLevelBlock />
+                <Box className={classes.content_help_block} ml="xs">
+                    <ProfileLevelBlock/>
                 </Box>
             </Box>
         </Box>
