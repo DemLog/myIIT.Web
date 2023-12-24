61c7b1b0c5fddae28a35e00183b2a387868a70ee
fremyxs <antonovch13@mail.ru>
2023-12-25 02:13:33 +0500

    feat: add messages component

---
+++ src/components/common/Messages/ChatRoom/ChatRoom.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mantine/core";
+import { Block } from "@components/UI";
+
+import classes from './ChatRoom.module.css'
+
+export const ChatRoom = () => <Box className={classes.chat_room}>
+    <Block />
+</Box>
